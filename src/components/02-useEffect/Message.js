d70f9be6667f23fc43b23081bf9cbeff18aa6e93
Edgard Vilo <thevilos@gmail.com>
2021-12-11 15:53:37 -0300

    clase 109

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,17 +1,29 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 const Message = () => {
 
+    const [coords, setCoors] = useState({ x: 0, y: 0});
+    const { x,y } = coords;
+
     useEffect(() => {
         console.log('componente montado');
+        const mouseMove = (e) => {
+            const coords = { x: e.x, y: e.y };
+            setCoors(coords);
+        }
+        window.addEventListener('mousemove', mouseMove);
         return () => {
             console.log('componente desmontado');
+            window.removeEventListener('mousemove', mouseMove);
         }
     }, [])
 
     return (
         <>
             <h3>Eres Genial</h3>
+            <p>
+                x: { x }, y: { y }
+            </p>
         </>
     )
 }
