093d61099f9d53579583ebe3b9bbd9cabfaf82c7
Edgard Vilo <thevilos@gmail.com>
2021-12-12 14:39:53 -0300

    clase 116: memo - Método de React

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // import RealExampleRef from './components/04-useRef/RealExampleRef';
-import Layout from './components/05-useLayoutEffect/LayoutEffect';
+import Memorize from './components/06-memos/Memorize';
 // import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks';
 // import FocusScreen from './components/04-useRef/FocusScreen';
 // import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
@@ -12,7 +12,7 @@ import Layout from './components/05-useLayoutEffect/LayoutEffect';
 // import HookApp from './HookApp';
 
 ReactDOM.render(
-    <Layout />,
+    <Memorize />,
   document.getElementById('root')
 );
 
