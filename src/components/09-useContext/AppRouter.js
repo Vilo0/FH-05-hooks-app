cab26a0cada10218a9c205b7822fbfa6caff207c
Edgard Vilo <thevilos@gmail.com>
2021-12-21 15:16:31 -0300

    clase 139: Link y NavLink

--- src/components/09-useContext/AppRouter.js
+++ src/components/09-useContext/AppRouter.js
@@ -5,6 +5,7 @@ import {
     Routes
 } from "react-router-dom";
 
+import NavBar from "./NavBar";
 import AboutScreen from "./AboutScreen";
 import HomeScreen from "./HomeScreen";
 import LoginScreen from "./LoginScreen";
@@ -13,10 +14,14 @@ const AppRouter = () => {
     return (
         <Router>
             <div>
+                <NavBar />
+
                 <Routes>
                     <Route path="" element={ <HomeScreen /> } />
                     <Route path="about" element={ <AboutScreen /> } />
                     <Route path="login" element={ <LoginScreen /> } />
+
+                    <Route path="*" element={ <HomeScreen /> } />
                 </Routes>
             </div>
         </Router>
