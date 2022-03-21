8c2e5225d01bfeab01e2813061422dc1b9d69076
Edgard Vilo <thevilos@gmail.com>
2022-03-20 21:28:57 -0300

    Clase 160: Pruebas de funciones del context

---
+++ src/__test__/components/09-useContext/LoginScreen.test.js
@@ -0,0 +1,27 @@
+import { mount } from 'enzyme';
+import LoginScreen from '../../../components/09-useContext/LoginScreen';
+import { UserContext } from '../../../components/09-useContext/UserContext';
+
+describe('Pruebas en <LoginScreen />', () => {
+    // wrapper // mount
+    const setUser = jest.fn();
+    const wrapper = mount(
+        <UserContext.Provider value={{
+            setUser
+        }}>
+            <LoginScreen />
+        </UserContext.Provider>
+    );
+
+    test('debe de mostrarse correctamente', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('debe de ejecutar el setUser con el argumento esperado', () => {
+        wrapper.find('button').simulate('click');
+        expect(setUser).toHaveBeenCalledWith({
+            id: 123,
+            name: 'Edgard Vilo'
+        });
+    });
+});
